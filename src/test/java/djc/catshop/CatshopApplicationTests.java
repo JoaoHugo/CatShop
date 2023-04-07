b74e37023c44d545c4b60e47400771916f11915f
João Hugo <joaohmarinho99@gmail.com>
2023-04-07 09:33:49 -0300

    commit inicial

---
+++ src/test/java/djc/catshop/CatshopApplicationTests.java
@@ -0,0 +1,13 @@
+package djc.catshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
